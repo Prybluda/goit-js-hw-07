3d08b944f38c1622e7249a091a4eb62584ebf747
Oleksandr Bry <134804799+Prybluda@users.noreply.github.com>
2023-09-29 16:36:34 +0200

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,41 +3,71 @@ import { galleryItems } from './gallery-items.js';
 
 // console.log(galleryItems);
 
+// const ul = document.querySelector('.gallery')
+// const li = galleryItems.map((item) => `
+// <li class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src=' ${item.preview}'
+//       data-source="${item.original}"
+//       alt=' ${item.description}'
+//     />
+//   </a>
+// </li>`).join('');
+// ul.insertAdjacentHTML('beforeend', li);
+
+// ul.addEventListener('click', openImg)
+// function openImg(evt) {
+//         evt.preventDefault();
+//     console.dir(evt)
+//     if (evt.target.nodeName !== 'IMG') {
+//         return;
+//     }
+
+//     const modal = basicLightbox.create(
+//         `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+
+//         {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
+//             onClose: () => window.removeEventListener('keydown', onEscKeyPress),
+//         }
+//     );
+    
+//     modal.show();
+
+//     function onEscKeyPress(evt) {   
+//         if (evt.code === "Escape") {
+//             modal.close();
+//         }
+//     }
+// }
+
 const ul = document.querySelector('.gallery')
-const li = galleryItems.map((item) => `
-<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src=' ${item.preview}'
-      data-source="${item.original}"
-      alt=' ${item.description}'
-    />
-  </a>
-</li>`).join('');
+const li = galleryItems.map(({original, preview, description}) =>
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+).join('');
 ul.insertAdjacentHTML('beforeend', li);
 
-ul.addEventListener('click', openImg)
-function openImg(evt) {
-        evt.preventDefault();
-    console.dir(evt)
-    if (evt.target.nodeName !== 'IMG') {
-        return;
-    }
+ul.addEventListener('click', openImg);
+function openImg(evt){
+    evt.preventDefault();
+if (evt.target.className !== 'gallery__image'){
+    return;
+}
 
-    const modal = basicLightbox.create(
-        `<img src="${evt.target.dataset.source}" width="800" height="600">`,
 
-        {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
-            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
-        }
-    );
-    
-    modal.show();
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
 
-    function onEscKeyPress(evt) {   
-        if (evt.code === "Escape") {
-            modal.close();
-        }
-    }
+instance.show()
 }
